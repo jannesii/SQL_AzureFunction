22418bd3c4c91f15fd7457d4d74c0f470ac09522
jannesi-projects <116154575+jannesi-projects@users.noreply.github.com>
2024-09-25 19:05:03 +0300

    SQL connection parameters from the route

--- TEST_SQL_HttpTrigger/__init__.py
+++ TEST_SQL_HttpTrigger/__init__.py
@@ -1,5 +1,7 @@
 import logging
-
+import os
+import pymssql
+import pandas as pd
 import azure.functions as func
 
 
@@ -7,13 +9,53 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     # Get the 'sql' parameter from the route
-    sql = req.route_params.get('sql')
-
-    if sql:
-        # Process the SQL value or send a response
-        return func.HttpResponse(f"SQL Parameter: {sql}")
-    else:
+    query = req.route_params.get('sqlquery')
+    
+    # Get SQL connection parameters from the route
+    server = f"{req.route_params.get('server')}.database.windows.net"
+    database = req.route_params.get('database')
+    username = req.route_params.get('username')
+    password = req.route_params.get('password')
+    
+    #return func.HttpResponse(f"{server, database, username, password, query}", status_code=200)
+    
+    if not all([server, database, username, password, query]):
         return func.HttpResponse(
-             "No SQL parameter was provided in the route.",
-             status_code=400
+            "Missing one or more environment variables (SERVER, DATABASE, USERNAME, PASSWORD, SQLQUERY).",
+            status_code=500
         )
+
+    try:
+        # Connect to the Azure SQL Database
+        conn = pymssql.connect(server=server, user=username,
+                               password=password, database=database)
+        cursor = conn.cursor()
+
+        # Execute the query
+        cursor.execute(query)
+
+        # Fetch all rows from the executed query
+        rows = cursor.fetchall()
+        return func.HttpResponse(f"Query result: {rows}", status_code=200)
+        
+        """
+        # Create a DataFrame from the query results
+        df = pd.DataFrame(rows, columns=[col[0] for col in cursor.description])
+
+        # Check if the DataFrame is empty
+        if df.empty:
+            return func.HttpResponse("No data found", status_code=200)
+
+        # Convert the DataFrame to a string for returning in the HTTP response
+        result_str = df.to_string(index=False)
+
+        return func.HttpResponse(f"Query result: \n\n{result_str}", status_code=200)
+        """
+    except Exception as e:
+        logging.error(f"Error occurred: {e}")
+        return func.HttpResponse(f"Error connecting to database: {str(e)}", status_code=500)
+
+    finally:
+        # Ensure the connection is closed
+        if 'conn' in locals():
+            conn.close()
