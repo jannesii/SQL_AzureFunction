b1b4c55395f212328117c1b934819051f4e5780f
jannesi-projects <116154575+jannesi-projects@users.noreply.github.com>
2024-09-25 19:24:08 +0300

    Get sql query from request body as json

--- TEST_SQL_HttpTrigger/__init__.py
+++ TEST_SQL_HttpTrigger/__init__.py
@@ -9,16 +9,23 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     # Get the 'sql' parameter from the route
-    query = req.route_params.get('sqlquery')
-    
+    # query = req.route_params.get('sqlquery')
+
+    # Extract the SQL query from the request body or query string
+    try:
+        req_body = req.get_json()
+        query = req_body.get('query')
+    except ValueError:
+        query = req.params.get('query')
+
     # Get SQL connection parameters from the route
     server = f"{req.route_params.get('server')}.database.windows.net"
     database = req.route_params.get('database')
     username = req.route_params.get('username')
     password = req.route_params.get('password')
-    
-    #return func.HttpResponse(f"{server, database, username, password, query}", status_code=200)
-    
+
+    # return func.HttpResponse(f"{server, database, username, password, query}", status_code=200)
+
     if not all([server, database, username, password, query]):
         return func.HttpResponse(
             "Missing one or more environment variables (SERVER, DATABASE, USERNAME, PASSWORD, SQLQUERY).",
@@ -36,9 +43,8 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
         # Fetch all rows from the executed query
         rows = cursor.fetchall()
-        #return func.HttpResponse(f"Query result: {rows}", status_code=200)
-        
-       
+        # return func.HttpResponse(f"Query result: {rows}", status_code=200)
+
         # Create a DataFrame from the query results
         df = pd.DataFrame(rows, columns=[col[0] for col in cursor.description])
 
@@ -50,7 +56,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         result_str = df.to_string(index=False)
 
         return func.HttpResponse(f"Query result: \n\n{result_str}", status_code=200)
-        
+
     except Exception as e:
         logging.error(f"Error occurred: {e}")
         return func.HttpResponse(f"Error connecting to database: {str(e)}", status_code=500)
